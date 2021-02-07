111a126050d536f448704d95e574ac9d18796452
rakisuto <tama5.bo6bo6@gmail.com>
2021-02-07 14:46:33 +0900

    何らかの修正

--- js/rotation.js
+++ js/rotation.js
@@ -3,147 +3,116 @@ const uAParser = UAParser();
 
 const $flip = $('.js-flip');
 const $flipInner = $('.js-flip-inner');
+const flipWidth = $flip.width();
 
-// コインフリップ関数
-const coinflip = () => {
+// 円の直径を180度に割り当てる
+const rate = 180 / flipWidth;
 
-  // イージング関数
-  let easing = (count, b, c, d, s) => {
-    if (s === undefined) s = 1.70158;
-    return c * ((count = count / d - 1) * count * ((s + 1) * count + s) + 1) + b;
-  };
+// .flip-innerクラスにつけるtransitionのduration値
+const DURATION = 800;
 
-  // 回転中の状態から元に戻す
-  let reset = () => {
-    $flipInner.css('transform', '');
-  };
+// touchMoveのフラグ
+let moving = false;
 
-  let frame = 20;
-  let count = 0;
-  let rotation = {
-    x: 0,
-    y: 0
-  };
-  // さわり始めた時のx座標とy座標を保存
-  let point = {
-    x: 0,
-    y: 0,
-    dx: 0,
-    dy: 0
-  };
-  let tmp = {
-    x: 0,
-    y: 0
-  };
-  let moving = false;
-  let animating = false;
+// touchEnd後、アニメーションしているときのフラグ
+let animating = false;
 
-  let touchStartHandler = (e) => {
-    if (uAParser.device.type !== 'mobile') return;
-    if (moving) return;
-    let touch = e.originalEvent.touches[0];
-    point.x = touch.screenX;
-    point.y = touch.screenY;
-    point.dx = 0;
-    point.dy = 0;
-    console.log('touchStartHandler', point.x);
-    console.log('touchStartHandler', point.y);
-    console.log('touchStartHandler', point.dx);
-    console.log('touchStartHandler', point.dy);
-  };
+// タップし始めたときのx座標
+let startPointX = 0;
 
-  let touchMoveHandler = (e) => {
-    if (uAParser.device.type !== 'mobile') return;
-    if (animating) return;
-    moving = true;
-    let touch = e.originalEvent.touches[0];
-    let x = (touch.screenX - point.x);
-    let y = (touch.screenY - point.y);
-    point.dx += x;
-    point.dy += y;
+// タップを離したときのx座標
+let endPointX = 0;
 
-    if (Math.abs(point.dx) > 10) {
-      e.preventDefault();
-    }
+// 現在の回転量
+let currentRotationVal = 0;
 
-    point.x = touch.screenX;
-    point.y = touch.screenY;
-    console.log('touchMoveHandler', point.x);
-    console.log('touchMoveHandler', point.y);
-    console.log('touchMoveHandler', point.dx);
-    console.log('touchMoveHandler', point.dy);
-
-    if (point.dx > 180) {
-      point.dx = 180;
-    } else if (point.dx < -180) {
-      point.dx = -180;
-    }
-
-    roll(point.dx);
-  };
-
-  let touchEndHandler = () => {
-    if (uAParser.device.type !== 'mobile') return;
-    if (!moving || animating) {
-      return;
-    }
-    moving = false;
-    point.x = point.y = 0;
-    tmp.x = rotation.x + point.dx;
-    tmp.y = rotation.y + point.dy;
-    console.log('touchEndHandler', point.x);
-    console.log('touchEndHandler', point.y);
-    console.log('touchEndHandler', point.dx);
-    console.log('touchEndHandler', point.dy);
-    if (point.dx > 45) {
-      tmp.lx = rotation.x + 180;
-    } else if (point.dx < -45) {
-      tmp.lx = rotation.x - 180;
-    } else {
-      tmp.lx = rotation.x;
-    }
-    animating = true;
-    tmp.ly = 0;
-    count = 0;
-    setTimeout(loopHandler, 16);
-  };
-
-  let loopHandler = () => {
-    let rx = easing(count, tmp.x, tmp.lx - tmp.x, frame - 1) - rotation.x;
-    let ry = easing(count, tmp.y, tmp.ly - tmp.y, frame - 1) - rotation.y;
-    if (count === frame) {
-      rotation.x += rx;
-      rotation.y += ry;
-      animating = moving = false;
-      return;
-    }
-    count++;
-    roll(rx);
-    setTimeout(loopHandler, 16);
-  };
-
-  // 計算した値を指定
-  let roll = (x) => {
-    $flipInner.css('transform', 'rotateY(' + (x + rotation.x) + 'deg)');
-  };
-
-  // Event
-  $flip.on('touchstart', touchStartHandler);
-  $flip.on('touchmove', touchMoveHandler);
-  $flip.on('touchend', touchEndHandler);
-
-  // リセット用イベント
-  $(window).on('resize', reset);
+// デバイスを判定
+let judgeDevice = () => {
+  (uAParser.device.type !== 'mobile') ? $flip.addClass('is-pc') : $flip.removeClass('is-pc');
 };
 
+// スワイプしはじめたとき
+let touchStartHandler = (e) => {
+  if (uAParser.device.type !== 'mobile') return;
+  if (moving) return;
+  let touch = e.originalEvent.touches[0];
+  // 触り始めたときのタップしたx,y座標を保存
+  startPointX = touch.screenX;
+};
+
+// スワイプ中
+let touchMoveHandler = (e) => {
+  if (uAParser.device.type !== 'mobile') return;
+  if (animating) return;
+  moving = true;
+  let touch = e.originalEvent.touches[0];
+
+  // スワイプ量を保存
+  endPointX = (touch.screenX - startPointX) * rate;
+
+  // 半回転以上回転させないように-180度以上、180度以下に制限
+  if (endPointX > 180 || endPointX < -180) {
+    return;
+  }
+
+  roll(currentRotationVal + endPointX);
+};
+
+// 指を離したとき
+let touchEndHandler = () => {
+  if (uAParser.device.type !== 'mobile') return;
+  if (!moving || animating) {
+    return;
+  }
+  moving = false;
+
+  // タップを離したとき、プラス方向に半分よりめくれていたら最後までめくる
+  if(currentRotationVal + endPointX > currentRotationVal + 90) {
+    currentRotationVal = currentRotationVal + 180;
+  }
+  // タップを離した時、マイナス方向に半分以上めくれていたら最後までめくる
+  if(currentRotationVal + endPointX < currentRotationVal - 90) {
+    currentRotationVal = currentRotationVal - 180;
+  }
+
+  // transitionしつつめくる
+  $flip.addClass('is-animating');
+  $flipInner.css('transform', 'rotateY(' + currentRotationVal + 'deg)');
+  animating = true;
+
+  setTimeout(() => {
+    // cssで設定したduration分動いたら、transitionを切る
+    $flip.removeClass('is-animating');
+    animating = false;
+  }, DURATION);
+
+  // スワイプし始めた位置を初期化
+  startPointX = 0;
+};
+
+// 回転
+let roll = (rotationVal) => {
+  $flipInner.css('transform', 'rotateY(' + (rotationVal) + 'deg)');
+};
+
+// リセット用イベント
+$(window).on('resize', judgeDevice);
+
+// イベント
+$flip.on('touchstart', touchStartHandler);
+$flip.on('touchmove', touchMoveHandler);
+$flip.on('touchend', touchEndHandler);
 $flip.on('mouseover', (e) => {
-  if (uAParser.device.type === 'mobile') return;
-  $(e.currentTarget).addClass('is-active');
+  if ($(e.currentTarget).hasClass('is-pc')) {
+    $(e.currentTarget).addClass('is-hover');
+  }
 });
 
 $flip.on('mouseout', (e) => {
-  if (uAParser.device.type === 'mobile') return;
-  $(e.currentTarget).removeClass('is-active');
+  if ($(e.currentTarget).hasClass('is-pc')) {
+    $(e.currentTarget).removeClass('is-hover');
+  }
 });
 
-coinflip();
+// 初期化
+judgeDevice();
